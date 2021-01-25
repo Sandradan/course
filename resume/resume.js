d225c5cd9e83fc1b97bd2d505853c2d2210f53fb
Sandradan <sandradan411@163.com>
2021-01-25 23:34:34 +0800

    add animation

--- resume/resume.js
+++ resume/resume.js
@@ -6,6 +6,21 @@ for (var i = 0; i < LiTags.length; i++) {
     }
     LiTags[i].onmouseleave = function (e) {
         e.currentTarget.classList.remove('active')
-       
+
+    }
+}
+
+
+
+
+let aTags = document.querySelectorAll('.navContainer>ul>li>a')
+for (let i = 0; i < aTags.length; i++) {
+    aTags[i].onclick = function (e) {
+        e.preventDefault()
+        let targets = e.currentTarget
+        let href = targets.getAttribute('href')
+        let element = document.querySelector(href)
+        let top = element.offsetTop;
+        window.scrollTo(0, top - 80)
     }
 }
