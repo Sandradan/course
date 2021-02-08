e7a41be3ece932ef6d546cd9a6fd659a515d9606
Sandradan <sandradan411@163.com>
2021-02-08 23:15:37 +0800

    learn jQuery2

--- jQuery/exercise.js
+++ jQuery/exercise.js
@@ -109,64 +109,68 @@
 
 
 
-window.jQuery = function (nodeOrSelector) {
-    let nodes = {}
-    if (typeof nodeOrSelector === 'string') {
-        let temp = document.querySelectorAll(nodeOrSelector)
-        for (let i = 0; i < temp.length; i++) {
-            nodes[i] = temp[i]
-        }
-        nodes.length = temp.length
-    } else if (nodeOrSelector instanceof Node) {
-        nodes = {
-            0: nodeOrSelector,
-            length: 1
-        }
-    }
+// window.jQuery = function (nodeOrSelector) {
+//     let nodes = {}
+//     if (typeof nodeOrSelector === 'string') {
+//         let temp = document.querySelectorAll(nodeOrSelector)
+//         for (let i = 0; i < temp.length; i++) {
+//             nodes[i] = temp[i]
+//         }
+//         nodes.length = temp.length
+//     } else if (nodeOrSelector instanceof Node) {
+//         nodes = {
+//             0: nodeOrSelector,
+//             length: 1
+//         }
+//     }
 
-    nodes.getSiblings = function () {
-        // var allchildren = node.parentNode.children
-        // var array = { length: 0 }
-        // for (let i = 0; i < allchildren.length; i++) {
-        //     if (allchildren[i] !== node) {
-        //         array[array.length] = allchildren[i]
-        //         array.length += 1
-        //     }
-        // }
-        // return array
-    }
-    nodes.addClass = function (classes) {
-        classes.forEach((value) => {
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].classList.add(value)
-            }
-        })
+//     nodes.getSiblings = function () {
+//         // var allchildren = node.parentNode.children
+//         // var array = { length: 0 }
+//         // for (let i = 0; i < allchildren.length; i++) {
+//         //     if (allchildren[i] !== node) {
+//         //         array[array.length] = allchildren[i]
+//         //         array.length += 1
+//         //     }
+//         // }
+//         // return array
+//     }
+//     nodes.addClass = function (classes) {
+//         classes.forEach((value) => {
+//             for (let i = 0; i < nodes.length; i++) {
+//                 nodes[i].classList.add(value)
+//             }
+//         })
 
-    }
+//     }
 
-    nodes.text = function (text) {
-        if (text === undefined) {
-            var texts = []
-            for (let i = 0; i < nodes.length; i++) {
-                texts.push(nodes[i].textContent)
-            }
-            return texts
-        } else {
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].textContent = text
-            }
-        }
-    }
+//     nodes.text = function (text) {
+//         if (text === undefined) {
+//             var texts = []
+//             for (let i = 0; i < nodes.length; i++) {
+//                 texts.push(nodes[i].textContent)
+//             }
+//             return texts
+//         } else {
+//             for (let i = 0; i < nodes.length; i++) {
+//                 nodes[i].textContent = text
+//             }
+//         }
+//     }
 
-    return nodes
-}
-
-
+//     return nodes
+// }
 
 
-var node2 = jQuery("ul>li")
-node2.getSiblings()
-console.log(node2.getSiblings())
-node2.addClass(['red'])
 
-node2.text('hi')
+
+// var node2 = jQuery("ul>li")
+// node2.getSiblings()
+// console.log(node2.getSiblings())
+// node2.addClass(['red'])
+
+// node2.text('hi')
+
+
+var nodes = jQuery('ul>li');
+nodes.addClass('red');
